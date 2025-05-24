ff91afeda1c50d27b55c1d917d0eefe60c8fbd63
Nishant Mishra <nishant.kumar.mishra@live.com>
2025-05-24 19:57:33 +0530

    Added signin page

---
+++ ManthanGurukul.BlazorApp/Models/SignInModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ManthanGurukul.BlazorApp.Models
+{
+    public class SignInModel
+    {
+        [Required(ErrorMessage = "Mobile No is required")]
+        [Range(1000000000, 9999999999, ErrorMessage = "Mobile No must be a 10-digit number")]
+        public string MobileNo { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Password is required")]
+        [MinLength(6, ErrorMessage = "Password must be at least 6 characters")]
+        public string Password { get; set; } = string.Empty;
+    }
+}
